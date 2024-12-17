80ef555938a2b0c7573823d693f107de3c5501ce
Saurav <sauravkumar00108@gmail.com>
2024-12-17 15:35:54 +0530

    first

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+app.use(express.static('public'));
+const io = socketio(server);
+
+app.set('view engine', 'ejs');
+app.set(express.static(path.join(__dirname, 'public'))); 
+
+io.on("connection" , function(socket)
+{
+    console.log("connected")
+    socket.on("send-location" , function(data){
+        io.emit("recive-location" , {id: socket.id , ...data});
+    });
+})
+
+app.get("/" , function(req, res){
+    res.render("index");
+});
+
+server.listen(3000);
---
+++ public/js/script.js
@@ -0,0 +1,36 @@
+const socket = io();
+
+if(navigator.geolocation)
+{
+    navigator.geolocation.watchPosition((possition) => {
+           const { latitude ,longitude} = possition.coords;
+           socket.emit("send-location" , {latitude , longitude});
+    },(error) => {
+        console.log(error , "while getting location");
+    },
+    {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000
+    });
+}
+
+const map = L.map("map").setView([0, 0], 15);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution: "Open map"
+}).addTo(map);
+
+const marker = {};
+
+socket.on("recive-location" , function(data){
+    const {id , latitude , longitude} = data;   
+    map.setView([latitude , longitude] , 15);
+    if(marker[id])
+    {
+        marker[id].setLatLng([latitude , longitude]);
+    }
+    else{
+        marker[id] = L.marker([latitude , longitude]).addTo(map);
+    }
+});
